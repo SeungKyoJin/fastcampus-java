cd973486681cb2fcf505013835d8706c6d74a266
seungkyojin <tmdry12322@gmail.com>
2021-10-14 21:44:43 +0900

    Initial commit

---
+++ src/main/java/com/example/exception/controller/ApiController.java
@@ -0,0 +1,37 @@
+package com.example.exception.controller;
+
+
+import com.example.exception.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("user")
+    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+
+        int a = 10 + age;
+        return user;
+    }
+
+    @PostMapping("user")
+    public User post(@Valid @RequestBody User user){
+        System.out.println(user);
+        return user;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
+        System.out.println("Api Controller");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
